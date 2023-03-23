36cf98bd467f61e7adf03c755aacc0d119aa8353
Cristobal Gonzalez <cristobal.gonzalezg@utem.cl>
2023-03-23 19:30:22 -0300

    Bloques de contenido para dinamizar la renderizacion de las paginas

--- core/views.py
+++ core/views.py
@@ -9,36 +9,22 @@ html_base = """
     <li><a href="/portfolio">Portafolio</a></li>
     <li><a href="/contact">Contacto</a></li>
 </ul>
-
 """
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "core/home.html")
+    return render(request, "core/home.html")  # dibuja el html
 
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2> Acerca de: </h2>
-        <p>Me llamo Criss </p>
-    """)
+    return render(request, "core/about.html")
 
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2> Portafolio </h2>
-        <h3>Proyectos: </h3>
-        <p>Proyecto 1 </p>
-        <p>Proyecto 2 </p>
-        <p>Proyecto 3 </p>
-    """)
+    return render(request, "core/portfolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2> Contacto </h2>
-        <h3>Datos: </h3>
-        <p>contacto 1 </p>
-    """)
+    return render(request, "core/contact.html")
